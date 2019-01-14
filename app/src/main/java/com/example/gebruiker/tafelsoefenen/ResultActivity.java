40d10c19e0e3cf836245836ab49b69a1a6ea5ed7
sannedonker <s.a.m.donker@hotmail.com>
2019-01-14 16:15:00 +0100

    Hellocharts library toegevoegd -- wel errors nu!

--- app/src/main/java/com/example/gebruiker/tafelsoefenen/ResultActivity.java
+++ app/src/main/java/com/example/gebruiker/tafelsoefenen/ResultActivity.java
@@ -1,7 +1,16 @@
 package com.example.gebruiker.tafelsoefenen;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        // example line chart (van de read me van hellocharts!)
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        LineChartView chart = new LineChartView(this);
+        chart.setLineChartData(data);
     }
 }
